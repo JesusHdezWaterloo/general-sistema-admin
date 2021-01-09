c84d2dd7bcb3cac735ebc6c1c8a543b068bd33e2
Unknown <unknown@example.com>
2021-01-09 03:52:14 -0500

    Externo

--- src/main/java/com/root101/sistema/admin/application/Main.java
+++ src/main/java/com/root101/sistema/admin/application/Main.java
@@ -16,22 +16,22 @@
  */
 package com.root101.sistema.admin.application;
 
-import com.jhw.module.util.company.ui.module.CompanySwingModule;
-import com.jhw.swing.bundles.loading.LoadingProcess;
-import com.jhw.swing.bundles.loading.LoadingWorker;
-import com.jhw.module.util.bug.module.BugSwingModule;
-import com.jhw.module.util.calc.CalcSwingModule;
-import com.jhw.swing.material.components.container.panel._PanelGradient;
+import com.root101.module.util.company.ui.module.CompanySwingModule;
+import com.root101.swing.bundles.loading.LoadingProcess;
+import com.root101.swing.bundles.loading.LoadingWorker;
+import com.root101.module.util.bug.module.BugSwingModule;
+import com.root101.module.util.calc.CalcSwingModule;
+import com.root101.swing.material.components.container.panel._PanelGradient;
 import javax.swing.JOptionPane;
-import com.jhw.module.util.default_config.DefaultConfigSwingModule;
+import com.root101.module.util.default_config.DefaultConfigSwingModule;
 import com.root101.module.control.licence.ui.module.LicenceSwingModule;
-import com.jhw.module.util.console.ConsoleSwingModule;
-import com.jhw.swing.material.standards.MaterialIcons;
-import com.jhw.module.util.authentication_manager.ui.module.AuthSwingModule;
-import com.jhw.module.util.local_server.ui.module.LocalServerSwingModule;
-import com.jhw.module.util.rest_config.ui.module.RestConfigSwingModule;
-import com.jhw.module.util.tec.TecSwingModule;
-import com.jhw.swing.material.components.splashScreen.SplashScreen;
+import com.root101.module.util.console.ConsoleSwingModule;
+import com.root101.swing.material.standards.MaterialIcons;
+import com.root101.module.util.authentication_manager.ui.module.AuthSwingModule;
+import com.root101.module.util.local_server.ui.module.LocalServerSwingModule;
+import com.root101.module.util.rest_config.ui.module.RestConfigSwingModule;
+import com.root101.module.util.tec.TecSwingModule;
+import com.root101.swing.material.components.splashScreen.SplashScreen;
 import com.root101.module.admin.kanban.ui.module.KanbanSwingModule;
 import com.root101.module.admin.seguridad.ui.module.SeguridadSwingModule;
 import com.root101.module.gestion.contabilidad.ui.module.ContabilidadSwingModule;
